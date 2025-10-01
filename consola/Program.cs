0e231b6b674e9a6d9d7e039e5a55cb1428922d1c
Juancho-115 <juanjogarcia115@gmail.com>
2025-10-01 01:30:39 -0500

    implementado el clicker en la version de consola

--- consola/Program.cs
+++ consola/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clicker clicker = new Clicker();
+
+            System.Console.WriteLine("Presiona Enter para hacer click o 'q' para terminar:");
+
+            while (true)
+            {
+                var tecla = System.Console.ReadKey(true);
+                if (tecla.Key == ConsoleKey.Enter)
+                {
+                    clicker.ProcesarClick();
+                    System.Console.WriteLine($"Cantidad de clicks: {clicker.ObtenerCantidadClicks()}");
+                }
+                else if (tecla.Key == ConsoleKey.Q)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
