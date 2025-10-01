fb771acb364af191e46fec967cb4eb0629d0e7fd
Juancho-115 <juanjogarcia115@gmail.com>
2025-10-01 02:01:09 -0500

    añadir clase Clicker con funcionalidad de conteo de clicks y logging

---
+++ unity/clicker/Assets/Scripts/Game/Clicker.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.Events;
+using clicker.Interfaces;
+using clicker.Singletons;
+
+namespace clicker.Game
+{
+    public class Clicker : MonoBehaviour, IClicker
+    {
+        private int _cantidadClicks = 0;
+
+        public Clicker()
+        {
+            _cantidadClicks = 0;
+        }
+
+        private void Start()
+        {
+            _cantidadClicks = 0;
+            Logger.Instance.Log("Clicker inicializado con 0 clicks.");
+        }
+        public void OnClick()
+        {
+            _cantidadClicks++;
+            Logger.Instance.Log($"Click registrado. Total clicks: {_cantidadClicks}");
+        }
+
+        public int ObtenerCantidadClicks()
+        {
+            return _cantidadClicks;
+        }
+
+        public void OnButtonClick()
+        {
+            OnClick();
+        }
+
+        public void ResetClicks()
+        {
+            _cantidadClicks = 0;
+            Logger.Instance.Log("Contador de clicks reseteado a 0.");
+        }
+
+        public string ObtenerClicksComoString()
+        {
+            return _cantidadClicks.ToString();
+        }
+    }
+}
