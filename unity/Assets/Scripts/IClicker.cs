fd0dd5f0c42bf26b9eec32e3aed86033acc62d5b
Juancho-115 <juanjogarcia115@gmail.com>
2025-10-14 19:00:32 -0500

    interfaces creadas

---
+++ unity/Assets/Scripts/IClicker.cs
@@ -0,0 +1,9 @@
+namespace Taller2.Clicker
+{
+    public interface IClicker
+    {
+        void ProcesarClick();
+        int ObtenerCantidadClicks();
+        void Reiniciar();
+    }
+}
---
+++ unity/Assets/Scripts/ILogger.cs
@@ -0,0 +1,7 @@
+namespace Taller2.Clicker
+{
+    public interface ILogger
+    {
+        void Log(string message);
+    }
+}
