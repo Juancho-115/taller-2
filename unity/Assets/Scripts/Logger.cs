25d07666504e3b688b7d67f3f15a7d1673cc2a1f
Juancho-115 <juanjogarcia115@gmail.com>
2025-10-14 19:07:24 -0500

    implementacion del logger

---
+++ unity/Assets/Scripts/Logger.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEngine;
+
+namespace Taller2.Clicker
+{
+    public sealed class Logger : ILogger
+    {
+        private static Logger instancia = null;
+
+        private Logger() { }
+
+        public static Logger Instancia
+        {
+            get
+            {
+                if (instancia == null)
+                {
+                    instancia = new Logger();
+                }
+                return instancia;
+            }
+        }
+
+        public void Log(string message)
+        {
+            var logMessage = $"{System.DateTime.Now:0}: {message}";
+            UnityEngine.Debug.Log(message);
+        }
+    }
+}
